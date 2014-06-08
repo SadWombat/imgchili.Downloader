c71b532d9b3a05daa1c30dc90fd7cd7f080e1181
SadWombat <Dmitry.Kuklin@gmail.com>
2014-06-08 23:01:02 +0200

    #1

---
+++ main.dfm
@@ -0,0 +1,106 @@
+object Form1: TForm1
+  Left = 0
+  Top = 0
+  BorderStyle = bsDialog
+  Caption = 'imgchili.Downloader'
+  ClientHeight = 333
+  ClientWidth = 488
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  Position = poDesktopCenter
+  OnClose = FormClose
+  PixelsPerInch = 96
+  TextHeight = 13
+  object Label1: TLabel
+    Left = 8
+    Top = 79
+    Width = 87
+    Height = 13
+    Caption = 'Founded Pictures:'
+  end
+  object Label2: TLabel
+    Left = 8
+    Top = 11
+    Width = 63
+    Height = 13
+    Caption = 'Album odres:'
+  end
+  object Label3: TLabel
+    Left = 8
+    Top = 47
+    Width = 89
+    Height = 13
+    Caption = 'Destination folder:'
+  end
+  object DownloadButton: TButton
+    Left = 406
+    Top = 299
+    Width = 75
+    Height = 25
+    Caption = 'Download'
+    TabOrder = 0
+    OnClick = DownloadButtonClick
+  end
+  object AlbumHTTPAdress: TEdit
+    Left = 103
+    Top = 8
+    Width = 378
+    Height = 21
+    TabOrder = 1
+    Text = 'http://imgchili.net/album/fa88732b77176dd2e72327476f95e0df'
+  end
+  object FoundenPics: TListBox
+    Left = 8
+    Top = 98
+    Width = 473
+    Height = 187
+    ItemHeight = 13
+    TabOrder = 2
+  end
+  object SaveFolder: TEdit
+    Left = 103
+    Top = 44
+    Width = 378
+    Height = 21
+    TabOrder = 3
+    Text = 'D:\Downloads\imgchili.net\'
+  end
+  object Button1: TButton
+    Left = 325
+    Top = 299
+    Width = 75
+    Height = 25
+    Caption = 'Cencel'
+    TabOrder = 4
+    OnClick = Button1Click
+  end
+  object IdHTTP: TIdHTTP
+    AllowCookies = True
+    ProxyParams.BasicAuthentication = False
+    ProxyParams.ProxyPort = 0
+    Request.ContentLength = -1
+    Request.ContentRangeEnd = -1
+    Request.ContentRangeStart = -1
+    Request.ContentRangeInstanceLength = -1
+    Request.Accept = 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'
+    Request.BasicAuthentication = False
+    Request.UserAgent = 
+      #39'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:12.0) Gecko/20100101 Fir' +
+      'efox/12.0'#39
+    Request.Ranges.Units = 'bytes'
+    Request.Ranges = <>
+    HTTPOptions = [hoForceEncodeParams]
+    CookieManager = IdCookieManager
+    Left = 24
+    Top = 120
+  end
+  object IdCookieManager: TIdCookieManager
+    Left = 96
+    Top = 120
+  end
+end
---
+++ main.pas
@@ -0,0 +1,134 @@
+unit main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, IdBaseComponent,
+  IdComponent, IdTCPConnection, IdTCPClient, IdHTTP, IdCookieManager, RegularExpressions;
+
+type
+  TForm1 = class(TForm)
+    IdHTTP: TIdHTTP;
+    DownloadButton: TButton;
+    AlbumHTTPAdress: TEdit;
+    FoundenPics: TListBox;
+    Label1: TLabel;
+    IdCookieManager: TIdCookieManager;
+    SaveFolder: TEdit;
+    Label2: TLabel;
+    Label3: TLabel;
+    Button1: TButton;
+    procedure DownloadButtonClick(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure FormClose(Sender: TObject; var Action: TCloseAction);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+TLoopThread = class(TThread)
+    private
+    { Private declarations }
+  protected
+    procedure Execute; override;
+  end;
+
+var
+  Form1: TForm1;
+  LoopThread: TLoopThread;
+  RegEx, RegEx2: TRegEx;
+
+implementation
+
+{$R *.dfm}
+
+
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  if not LoopThread.Terminated then
+    if MessageDlg('Currently in progress. Are you want terminate downloading files?', mtConfirmation, [mbYes, mbNo], 0)=mrYes then
+    begin
+      LoopThread.Terminate;
+    end;
+end;
+
+procedure TForm1.DownloadButtonClick(Sender: TObject);
+var
+  FS:TFileStream;
+  s: string;
+begin
+  FoundenPics.Clear;
+
+  RegEx:=TRegEx.Create('http:\/\/[a-z][0-9].imgchili.net\/[0-9]*\/\w*.jpg');
+  RegEx2:=TRegEx.Create('\w*.jpg');
+
+  if FileExists('data.tmp') then DeleteFile('data.tmp');
+  FS:=TFileStream.Create('data.tmp', fmCreate);
+  IdHTTP.ConnectTimeout:=1000;
+  IdHTTP.Get(AlbumHTTPAdress.Text,FS);
+  FS.Free;
+
+  if not DirectoryExists(SaveFolder.Text) then
+    CreateDir(SaveFolder.Text);
+  s:=SaveFolder.Text;
+  if s[Length(s)]<>'\' then
+  begin
+    Insert('\', s, Length(s)+1);
+    SaveFolder.Text:=s;
+  end;
+
+  LoopThread:=TLoopThread.Create(False);
+  LoopThread.Priority:=tpNormal;
+end;
+
+procedure TForm1.FormClose(Sender: TObject; var Action: TCloseAction);
+begin
+  LoopThread.Free;
+end;
+
+procedure TLoopThread.Execute;
+var
+  i:Integer;
+  s2, sLine, ImgName:string;
+  F: TextFile;
+  FS:  TFileStream;
+  LoopBreaked: Boolean;
+begin
+  if FileExists('data.tmp') then
+  begin
+    Assign(F, 'data.tmp');
+    Reset(f);
+    LoopBreaked:=False;
+    while not Eof(f) do
+    if not Terminated then
+    begin
+      Readln(f, sLine);
+      if RegEx.IsMatch(sLine) then
+      begin
+        sLine:=RegEx.Match(sLine).Value;
+        sLine[8]:='i';
+        Form1.FoundenPics.Items.Add(sLine);
+        Form1.Label1.Caption:='Founded '+IntToStr(Form1.FoundenPics.Count)+' pictures!';
+        ImgName:=RegEx2.Match(sLine).Value;
+        if not FileExists(Form1.SaveFolder.Text+ImgName) then
+        begin
+          FS:=TFileStream.Create(Form1.SaveFolder.Text+ImgName,fmCreate);
+          Form1.IdHTTP.Get(sLine,FS);
+          FS.Free;
+        end;
+      end;
+    end else
+    begin
+      LoopBreaked:=True;
+      Break;
+    end;
+  end;
+  LoopThread.Terminate;
+  if LoopBreaked=False then
+    MessageDlg('All images downloaded', mtInformation, [mbOK], 0);
+  CloseFile(F);
+  end;
+end.
